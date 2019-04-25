fd7a56501ddf59c98f7a37515ace748b7d8d2d43
Jim Bennett <jim.bennett@microsoft.com>
2019-04-26 00:57:42 +0100

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,10 +1,1 @@
-# Create pipes for the audio and video streams
-mkfifo /tmp/streamaudio.wav
-mkfifo /tmp/streamvideo
-
-# Pipe the audio to the audio pipe
-# pipe the video to the video pipe
-# Combine these pipes and stream to ffmpeg
-arecord -D plughw:1 -f s16 -r 44100 -d 0 --buffer-size=1000000 > /tmp/streamaudio.wav &
-raspivid -n -t 0 -w 1280 -h 720 -fps 12 -b 250000 -o - > /tmp/streamvideo &
-ffmpeg -thread_queue_size 4096 -i /tmp/streamvideo -thread_queue_size 4096 -itsoffset 15.0 -i /tmp/streamaudio.wav -acodec libmp3lame -codec copy -strict experimental -acodec mp3 -ar 44100 -b:a 128k -b:v 1500k -threads 2 -f flv $1
+raspivid -n -t 0 -w 1280 -h 720 -fps 25 -g 50 -b 250000 -o - | ffmpeg -r 25.37 -i - -f alsa -ar 22050 -ac 1 -itsoffset 15.0 -i hw:1,0 -c:v copy -r 25 -vsync 1 -c:a mp3 -f flv $1
